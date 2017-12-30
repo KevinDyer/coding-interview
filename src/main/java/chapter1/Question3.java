02ee8d481e453a0f706d93285a7e19cf940020b9
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-12-29 19:28:48 -0700

    refactor: changed spacing

--- src/main/java/chapter1/Question3.java
+++ src/main/java/chapter1/Question3.java
@@ -9,17 +9,17 @@ public class Question3 {
   }
   
   public static void urlify(char[] str, int length) {
-  	int i = length - 1;
-  	int tail = str.length - 1;
-  	while (i >= 0) {
-  		if (str[i] == ' ') {
-  			str[tail--] = '0';
-  			str[tail--] = '2';
-  			str[tail--] = '%';
-  		} else {
-  			str[tail--] = str[i];
-  		}
-  		i--;
-  	}
+    int i = length - 1;
+    int tail = str.length - 1;
+    while (i >= 0) {
+      if (str[i] == ' ') {
+        str[tail--] = '0';
+        str[tail--] = '2';
+        str[tail--] = '%';
+      } else {
+        str[tail--] = str[i];
+      }
+      i--;
+    }
   }
 }
